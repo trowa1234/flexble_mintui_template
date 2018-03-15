daa9e2c249985c037a0b13888e5abc4adfab061b
kidd.lee <13551654@qq.com>
2018-03-15 17:07:59 +0800

    flexble+第三方库

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <demo></demo>
+    <router-view/>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+import demo from './components/demo/demo'
+export default {
+  name: 'App',
+  components:{
+    demo
+  }
+}
+</script>
+
+<style scoped lang='less'>
+#app {
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
